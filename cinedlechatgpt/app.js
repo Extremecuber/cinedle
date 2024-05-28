5eb2796a86b7d68280678811bd153f0c5ec99265
Extremecuber <102213861+Extremecuber@users.noreply.github.com>
2024-05-29 04:11:51 +0530

    Update app.js

--- cinedlechatgpt/app.js
+++ cinedlechatgpt/app.js
@@ -18,7 +18,7 @@ server.listen(PORT, () => {
 
 const { MongoClient } = require('mongodb');
 
-const uri = 'mongodb://27017/Cinedle';
+const uri = 'mongodb://localhost:27017/Cinedle';
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function connectToMongoDB() {
