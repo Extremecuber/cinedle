dc0d1c65994683ab5e19e5cbb2d2a6f7b922895b
Extremecuber <102213861+Extremecuber@users.noreply.github.com>
2024-06-01 01:34:22 +0530

    Update app.js

--- cinedlechatgpt/app.js
+++ cinedlechatgpt/app.js
@@ -1,15 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const { MongoClient } = require('mongodb');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000; // Use port 3000 for HTTP
+const port = process.env.PORT || 3000;
 
-const uri = 'mongodb://localhost:27017'; // Connection string
-const dbName = 'Cinedle'; // Replace with your database name
+const uri = 'mongodb://localhost:27017';  // Connection string
+const dbName = 'Cinedle';
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 let db;
 
@@ -21,18 +22,39 @@ async function connectToMongoDB() {
         console.log('Connected to MongoDB');
     } catch (error) {
         console.error('Failed to connect to MongoDB', error);
-        process.exit(1); // Exit process with a failure code
+        process.exit(1);
     }
 }
 
 connectToMongoDB();
 
-// Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Serve the index.html file when accessing the root URL
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/connect-to-mongodb', async (req, res) => {
+    try {
+        if (!db) await connectToMongoDB();
+        res.status(200).send('Connected to MongoDB');
+    } catch (error) {
+        res.status(500).send('Failed to connect to MongoDB');
+    }
+});
+
+app.get('/get-images', async (req, res) => {
+    try {
+        const collections = await db.listCollections().toArray();
+        const randomCollection = collections[Math.floor(Math.random() * collections.length)].name;
+        const images = await db.collection(randomCollection).find().toArray();
+        const imagePaths = images.map(image => image.path); // Assuming each image document has a 'path' field
+
+        res.json({ correctMovie: randomCollection, frames: imagePaths });
+    } catch (err) {
+        console.error('Failed to fetch images from MongoDB', err);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 app.listen(port, () => {
