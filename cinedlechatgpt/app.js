4280247236fd83b66e94581ca002fb2f1173c610
Extremecuber <102213861+Extremecuber@users.noreply.github.com>
2024-05-31 22:59:39 +0530

    Update app.js | Removed comments

--- cinedlechatgpt/app.js
+++ cinedlechatgpt/app.js
@@ -1,39 +1,3 @@
-// const http = require('http');
-// const fs = require('fs');
-
-// // Read the contents of script.js
-// const scriptContent = fs.readFileSync('./script.js', 'utf8');
-
-// // Create an HTTP server
-// const server = http.createServer((req, res) => {
-//     res.writeHead(200, {'Content-Type': 'text/javascript'});
-//     res.end(scriptContent);
-// });
-
-// // Listen on port 3000
-// const PORT = process.env.PORT || 3000;
-// server.listen(PORT, () => {
-//     console.log(`Server running on port ${PORT}`);
-// });
-
-// const { MongoClient } = require('mongodb');
-
-
-// const uri = 'mongodb://16.171.70.50:27017/Cinedle';
-
-// const client = new MongoClient(uri);
-
-// async function connectToMongoDB() {
-//     try {
-//         await client.connect();
-//         console.log('Connected to MongoDB');
-//     } catch (err) {
-//         console.error('Failed to connect to MongoDB', err);
-//     }
-// }
-
-// connectToMongoDB();
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const { MongoClient } = require('mongodb');
